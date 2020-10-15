df7678437c73404cd926e0c900221966b2d4a1b1
TJ McCarty <tj@Arcitechx.ninja>
2020-10-15 11:38:32 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  // Would need to make the loading between screens happen in the angular app since this is a single page application.
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+  }
+
+  hideSpinner() {
+    this.setState({ visible: false });
+  }
+
+  showSpinner() {
+    this.setState({ visible: true });
+  }
+
+  render() {
+    const url = "http://157.230.11.18:7777/";
+
+    return (
+        <View style={styles.view}>
+          <WebView 
+            style={styles.webview}
+            source={{ uri: url }} 
+            onNavigationStateChange={(event) => {
+              if(event.url !== url) {
+                this.hideSpinner()
+              } else {
+                this.showSpinner();
+              }
+            }}
+            onLoad={() => this.hideSpinner()}
+            />
+          {this.state.visible && (
+            <ActivityIndicator
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+              }}
+              color="#1884BA"
+              size="large"
+            />
+          )}
+          {/* <ActivityIndicator
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+              }}
+              color="#1884BA"
+              size="large"
+            /> */}
+        </View>
+             
+    );
+  }
+ 
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+  view: {
     flex: 1,
+  },
+  webview: {
+    height: "100%",
+    width: "100%",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    marginTop: 20,
+  }
 });
