59f72f58dc62c90d81900e131864db7e93c89afb
TJ McCarty <tj@Arcitechx.ninja>
2021-03-19 15:29:44 -0500

    update

--- App.js
+++ App.js
@@ -3,50 +3,13 @@ import { StyleSheet, ActivityIndicator, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 class App extends Component {
-  // Would need to make the loading between screens happen in the angular app since this is a single page application.
-  constructor(props) {
-    super(props);
-    this.state = { visible: true };
-  }
-
-  hideSpinner() {
-    this.setState({ visible: false });
-  }
-
-  showSpinner() {
-    this.setState({ visible: true });
-  }
-
   render() {
-    const url = "http://157.230.11.18:7777/";
-    const loggedInUrl = "http://157.230.11.18:7777/latest";
-
     return (
         <View style={styles.view}>
           <WebView 
             style={styles.webview}
-            source={{ uri: url }} 
-            onNavigationStateChange={(event) => {
-              if(event.url !== loggedInUrl) {
-                this.showSpinner();
-                // this.hideSpinner();
-              }
-            }}
-            onLoad={() => this.hideSpinner()}
+            source={{ uri: "http://157.230.11.18:7777/" }} 
             />
-          {this.state.visible && (
-            <ActivityIndicator
-              style={{
-                position: "absolute",
-                left: 0,
-                right: 0,
-                bottom: 0,
-                top: 0,
-              }}
-              color="#1884BA"
-              size="large"
-            />
-          )}
         </View>
              
     );
---
+++ AppOld.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { StyleSheet, ActivityIndicator, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+class App extends Component {
+  // Would need to make the loading between screens happen in the angular app since this is a single page application.
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+  }
+
+  // need to turn back on the time out hidespinner after like 3 seconds now., 
+  
+  // or completely remove spinner since page loads fast!!!!
+  
+  hideSpinner() {
+    this.setState({ visible: false });
+  }
+
+  showSpinner() {
+    this.setState({ visible: true });
+  }
+
+  render() {
+    const url = "http://157.230.11.18:7777/";
+    const loggedInUrl = "http://157.230.11.18:7777/latest";
+
+    return (
+        <View style={styles.view}>
+          <WebView 
+            style={styles.webview}
+            source={{ uri: url }} 
+            onNavigationStateChange={(event) => {
+              if(event.url !== loggedInUrl) {
+                this.showSpinner();
+                // this.hideSpinner();
+              }
+            }}
+            onLoad={() => this.hideSpinner()}
+            />
+          {this.state.visible && (
+            <ActivityIndicator
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 0,
+                top: 0,
+              }}
+              color="#1884BA"
+              size="large"
+            />
+          )}
+        </View>
+             
+    );
+  }
+ 
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+  webview: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  }
+});
